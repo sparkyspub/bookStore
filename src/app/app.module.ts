6d230fd3a5c418c8d7d04a34119fba9c50b49c44
Jorge Carmona <jcarmonausf@gmail.com>
2018-04-04 23:00:09 -0500

    Added new methods for book api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from '../books/books-list/books-list.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { TruncatePipe } from './shared/pipes/truncate.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
--- src/books/books-list/books-list.component.ts
+++ src/books/books-list/books-list.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges} from '@angular/core';
+// import { IBook } from '../book';
+// interface IBook {
+//   bookAuthor: string;
+//   bookTitle: string;
+//   bookPrice: number;
+//   bookDescription: string;
+//   publishedOn: Date;
+//   inStock: string;
+//   bookReviews: number;
+//   hardcover?: boolean;
+// }
 
 @Component({
   moduleId: module.id,
   selector: 'bs-books-list',
-  templateUrl: 'books-list.component.html'
+  templateUrl: 'books-list.component.html',
+  styleUrls: ['../../app/app.component.css']
 })
 
-export class BooksListComponent {
+export class BooksListComponent implements OnInit, OnChanges {
+  ngOnInit(){
+    console.log('Init', this.booksInStock);
+  }
+  ngOnChanges(){
+    console.log('new change detected');
+  }
+
+  changeMethod(): void {
+    this.animals= ['dog', 'cat'];
+    console.log('change method happened', this.animals);
+  }
+
+  animals: string[] = ['zebra', 'moose'];
   showImage : boolean = true;
   booksInStock :number = 2
+
+  books: any[]= [{
+    bookAuthor: "Tom Jones",
+    bookTitle: "War and Peace 2",
+    bookPrice: 29.95,
+    bookDescription:"Book of historical fiction",
+    publishedOn: new Date('02/11/1921'),
+    inStock: "yes",
+    bookReviews: 15,
+  }]
 
   toggleImage():void {
     this.showImage = !this.showImage;
